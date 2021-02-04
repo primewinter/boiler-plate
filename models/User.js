7eec9de5fd52133ded54d48ff6738a160583d2f8
primewinter <yuuni.cha@gmail.com>
2021-02-05 00:04:09 +0900

    로그인 구현

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
+const dev = require('../config/dev');
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -30,6 +34,49 @@ const userSchema = mongoose.Schema({
         tupe: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')) {
+        //비밀번호를 암호화시킨다
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword 1234567 암호화된비밀번호 $2b$10$V8KzRE7KtULDrRnhAMJAP.pUdcrdrUju4F/L7Gd1ygD6M5ITgLQWG
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    // jsonwebtoken을 이용해서 token 생성
+    var token = jwt.sign(user._id.toHexString(), dev.jwtToken)
+    // user._id + jwtToken = token
+    // ->
+    // jwtToken -> user._id
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
